fd501c6ca9381580ff35b783457d27e91cb995c3
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-10-11 21:55:28 +0800

    Sets up initial task manager layout
    Sets up the basic layout for the task manager application.
    
    - Updates metadata for title and description.
    - Integrates Space Mono font.
    - Adds a header component.
    - Introduces a modal for adding tasks.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
-import Image from "next/image";
+"use client"
+import AddTaskModal from "@/components/add-task-modal/AddTaskModal";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  }
+
+  const handleOutClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      handleModalClose();
+    }
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-cols-4  items-center justify-center min-h-screen w-full">
+      {
+        isModalOpen ? 
+        <div className="absolute flex items-center justify-center top-0 left-0 z-100 h-screen w-full backdrop-blur-sm" onClick={handleOutClick}>
+          <AddTaskModal isOpen={isModalOpen} onClose={handleModalClose}/>
+        </div> : <></>
+      }
       
     </div>
   );
