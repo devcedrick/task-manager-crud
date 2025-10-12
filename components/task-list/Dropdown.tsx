e0d2e54e7ee38d2bf1810703d19e5aa428229e08
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-10-12 19:34:52 +0800

    Improves dropdown menu styling
    Enhances the dropdown menu's appearance with responsive width adjustments and updated button styles for better user experience.

--- components/task-list/Dropdown.tsx
+++ components/task-list/Dropdown.tsx
@@ -8,12 +8,12 @@ interface DropdownProps {
 
 const Dropdown: React.FC<DropdownProps> = ({onEdit, onDelete}) => {
   return (
-    <div className='absolute flex flex-col gap-2 right-7 z-100 top-12 w-50 bg-surface-2 border border-border rounded-md p-4'> 
-      <button type='button' className='w-full hover:text-background button hover:bg-accent' onClick={onEdit}>
+    <div className='absolute flex flex-col gap-2 right-7 z-100 top-12 w-40 lg:w-50 bg-surface-2 border border-border rounded-md p-4 '> 
+      <button type='button' className='w-full bg-accent text-background max-lg:text-sm lg:bg-transparent  hover:text-background button justify-start! hover:bg-accent' onClick={onEdit}>
         <Edit className='inline mr-3' size={16}/>
         Edit
       </button>
-      <button type='button' className='w-full hover:text-background button hover:bg-red-400' onClick={onDelete}>
+      <button type='button' className='w-full bg-red-400 text-background max-lg:text-sm lg:bg-transparent hover:text-background button justify-start! hover:bg-red-400' onClick={onDelete}>
         <Trash className='inline mr-3' size={16}/>
         Delete
       </button>
